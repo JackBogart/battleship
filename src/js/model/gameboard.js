3a912eb0a42c39751d5fdbbb60dc38ff7b2c96e1
Jack Bogart <jackbogart0822@gmail.com>
2024-10-13 15:48:47 -0400

    Update gameboard tests and separate methods for validating placement

--- src/js/model/gameboard.js
+++ src/js/model/gameboard.js
@@ -17,19 +17,9 @@ export function createGameboard() {
     .fill(null)
     .map(() => new Array(cols).fill(TileInfo.UNKNOWN));
 
-  const getShip = (row, col) => {
-    if (row < 0 || row >= rows || col < 0 || col >= cols) {
-      throw new Error('Cannot get ship out of bounds');
-    }
+  const getShip = (row, col) => board[row][col];
 
-    return board[row][col];
-  };
-
-  const setShip = (ship, row, col, isVertical = false) => {
-    if (row < 0 || row >= rows || col < 0 || col >= cols) {
-      throw new Error('Cannot place ship out of bounds');
-    }
-
+  const isValidPlacement = (ship, row, col, isVertical) => {
     const shipLength = ship.getLength();
     if (isVertical) {
       if (row + shipLength > rows) {
@@ -49,7 +39,12 @@ export function createGameboard() {
       }
     }
 
-    // Place the ship on the board
+    return true;
+  };
+
+  const setShip = (ship, row, col, isVertical) => {
+    const shipLength = ship.getLength();
+
     for (let i = 0; i < shipLength; i++) {
       const currentRow = isVertical ? row + i : row;
       const currentCol = isVertical ? col : col + i;
@@ -57,30 +52,18 @@ export function createGameboard() {
       board[currentRow][currentCol] = ship;
     }
     ships.push(ship);
-
-    return true;
   };
 
   const getInfoBoard = () => infoBoard.map((row) => [...row]);
   const getTileInfo = (row, col) => infoBoard[row][col];
 
   const receiveAttack = (row, col) => {
-    if (row < 0 || row >= rows || col < 0 || col >= cols) {
-      throw new Error('Cannot attack ship out of bounds');
-    }
-
-    if (infoBoard[row][col] !== TileInfo.UNKNOWN) {
-      return false;
-    }
-
     if (board[row][col] === null) {
       infoBoard[row][col] = TileInfo.MISSED;
     } else {
       board[row][col].hit();
       infoBoard[row][col] = TileInfo.HIT;
     }
-
-    return true;
   };
 
   const isFleetSunk = () => ships.every((ship) => ship.isSunk());
@@ -92,5 +75,6 @@ export function createGameboard() {
     getTileInfo,
     receiveAttack,
     isFleetSunk,
+    isValidPlacement,
   };
 }
--- src/js/model/gameboard.test.js
+++ src/js/model/gameboard.test.js
@@ -29,24 +29,9 @@ describe('getShip', () => {
   it('should return a ship if there is a ship at the location', () => {
     const ship = createShip(1);
 
-    gameboard.setShip(ship, 1, 1);
+    gameboard.setShip(ship, 1, 1, false);
 
     expect(gameboard.getShip(1, 1)).toBe(ship);
-  });
-
-  it('should throw an error if the location is invalid', () => {
-    expect(() => gameboard.getShip(15, 1)).toThrow(
-      'Cannot get ship out of bounds',
-    );
-    expect(() => gameboard.getShip(1, 10)).toThrow(
-      'Cannot get ship out of bounds',
-    );
-    expect(() => gameboard.getShip(-12, 1)).toThrow(
-      'Cannot get ship out of bounds',
-    );
-    expect(() => gameboard.getShip(1, -1)).toThrow(
-      'Cannot get ship out of bounds',
-    );
   });
 });
 
@@ -54,33 +39,15 @@ describe('setShip', () => {
   it('should place a ship at the specified location', () => {
     const ship = createShip(1);
 
-    const isValidPlacement = gameboard.setShip(ship, 1, 1);
+    gameboard.setShip(ship, 1, 1, false);
 
-    expect(isValidPlacement).toBe(true);
     expect(gameboard.getShip(1, 1)).toBe(ship);
-  });
-
-  it('should throw an error if the location is invalid', () => {
-    const ship = createShip(1);
-
-    expect(() => gameboard.setShip(ship, 15, 1)).toThrow(
-      'Cannot place ship out of bounds',
-    );
-    expect(() => gameboard.setShip(ship, 1, 10)).toThrow(
-      'Cannot place ship out of bounds',
-    );
-    expect(() => gameboard.setShip(ship, -12, 1)).toThrow(
-      'Cannot place ship out of bounds',
-    );
-    expect(() => gameboard.setShip(ship, 1, -1)).toThrow(
-      'Cannot place ship out of bounds',
-    );
   });
 
   it('should place a ship horizontally at specified location', () => {
     const ship = createShip(3);
 
-    gameboard.setShip(ship, 1, 1);
+    gameboard.setShip(ship, 1, 1, false);
 
     expect(gameboard.getShip(1, 1)).toBe(ship);
     expect(gameboard.getShip(1, 2)).toBe(ship);
@@ -98,11 +65,13 @@ describe('setShip', () => {
     expect(gameboard.getShip(3, 1)).toBe(ship);
     expect(gameboard.getShip(4, 1)).not.toBe(ship);
   });
+});
 
+describe('isValidPlacement', () => {
   it('should return false when the ship is partially out of bounds', () => {
     const ship = createShip(3);
 
-    const isValidPlacement = gameboard.setShip(ship, 9, 9);
+    const isValidPlacement = gameboard.isValidPlacement(ship, 9, 9);
 
     expect(isValidPlacement).toBe(false);
   });
@@ -112,17 +81,19 @@ describe('setShip', () => {
     const ship2 = createShip(3);
     gameboard.setShip(ship1, 2, 1, false);
 
-    const isValidPlacement = gameboard.setShip(ship2, 1, 2, true);
+    const isValidPlacement = gameboard.isValidPlacement(ship2, 1, 2, true);
 
     expect(isValidPlacement).toBe(false);
   });
+});
 
+describe('infoBoard', () => {
   it('should return a 10x10 board with unknown tiles', () => {
     const infoBoard = gameboard.getInfoBoard();
 
     expect(
       infoBoard.every((row) => row.every((tile) => tile === TileInfo.UNKNOWN)),
-    );
+    ).toBe(true);
   });
 
   it("should return a tile that isn't hit", () => {
@@ -132,10 +103,9 @@ describe('setShip', () => {
   });
 
   it('should mark an attacked empty tile as missed', () => {
-    const attacked = gameboard.receiveAttack(1, 1);
+    gameboard.receiveAttack(1, 1);
     const tileInfo = gameboard.getTileInfo(1, 1);
 
-    expect(attacked).toBe(true);
     expect(tileInfo).toBe(TileInfo.MISSED);
   });
 
@@ -143,26 +113,14 @@ describe('setShip', () => {
     const ship = createShip(3);
     gameboard.setShip(ship, 1, 1, false);
 
-    const attacked = gameboard.receiveAttack(1, 1);
+    gameboard.receiveAttack(1, 1);
     const tileInfo = gameboard.getTileInfo(1, 1);
 
-    expect(attacked).toBe(true);
     expect(tileInfo).toBe(TileInfo.HIT);
   });
+});
 
-  it('should return false when attacking a marked tile', () => {
-    const ship = createShip(3);
-    gameboard.setShip(ship, 1, 1, false);
-    gameboard.receiveAttack(1, 1);
-    gameboard.receiveAttack(1, 2);
-
-    const attack1 = gameboard.receiveAttack(1, 1);
-    const attack2 = gameboard.receiveAttack(1, 2);
-
-    expect(attack1).toBe(false);
-    expect(attack2).toBe(false);
-  });
-
+describe('isFleetSunk', () => {
   it('should return false when not all ships are sunk', () => {
     const ship1 = createShip(1);
     const ship2 = createShip(1);
@@ -174,21 +132,5 @@ describe('setShip', () => {
     const isFleetSunk = gameboard.isFleetSunk();
 
     expect(isFleetSunk).toBe(false);
-  });
-
-  it('should return true when all ships are sunk', () => {
-    const ship = createShip(1);
-    ship.isSunk.mockReturnValue(true);
-    gameboard.setShip(ship, 1, 1, false);
-
-    const isFleetSunk = gameboard.isFleetSunk();
-
-    expect(isFleetSunk).toBe(true);
-  });
-
-  it('should throw an error if the attacked location is invalid', () => {
-    expect(() => gameboard.receiveAttack(-1, 1)).toThrow(
-      'Cannot attack ship out of bounds',
-    );
   });
 });
